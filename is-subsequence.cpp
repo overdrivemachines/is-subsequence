98293a53c4be9ff7674102ccb0ccd9f7f7570185
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-01 22:14:04 -0700

    added basic info

--- is-subsequence.cpp
+++ is-subsequence.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+bool isSubsequence(string s, string t);
+
 int main(int argc, char const *argv[]) {
+  string s1 = "abc", t1 = "ahbgdc";  // true
+  string s2 = "axc", t2 = "ahbgdc";  // false
+
+  cout << s1 << ":" << t1 << ":" << isSubsequence(s1, t1) << endl;
+  cout << s2 << ":" << t2 << ":" << isSubsequence(s2, t2) << endl;
   return 0;
+}
+
+bool isSubsequence(string s, string t) {
+  return true;
 }
